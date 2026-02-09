c23265ee033d8669166557be03828fe4ac8de36b
Ammar Kafle <ammarkafle.edu@gmail.com>
2026-02-09 11:27:37 +0545

    updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,7 @@
-// Simple animation for skill bars on scroll
-const skills = document.querySelectorAll(".progress div");
+const toggleBtn = document.getElementById("themeToggle");
+const body = document.body;
 
-window.addEventListener("scroll", () => {
-  const trigger = window.innerHeight * 0.8;
-  skills.forEach(skill => {
-    const top = skill.getBoundingClientRect().top;
-    if (top < trigger) {
-      skill.style.opacity = 1;
-    }
-  });
+toggleBtn.addEventListener("click", () => {
+    body.classList.toggle("light");
+    toggleBtn.textContent = body.classList.contains("light") ? "☀️" : "🌙";
 });
