dc125d029ce6414933f67d083651bfa2fb31dadc
Ammar Kafle <ammarkafle.edu@gmail.com>
2026-02-09 11:32:28 +0545

    updated code

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.getElementById("themeToggle");
-const body = document.body;
-
-toggleBtn.addEventListener("click", () => {
-    body.classList.toggle("light");
-    toggleBtn.textContent = body.classList.contains("light") ? "☀️" : "🌙";
-});
